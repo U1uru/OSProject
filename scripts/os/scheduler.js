1e269ee74391319eddc09afc13ef0e1e4f875871
Jack Cooney <jcooney7722@Jack-Cooneys-MacBook-Pro.local>
2013-12-18 20:31:36 -0500

    implemented swapping

--- scripts/os/scheduler.js
+++ scripts/os/scheduler.js
@@ -10,17 +10,33 @@ function scheduler()
    this.contextSwitch = function(){
       if(_ReadyQueue.getSize() > 0){
          krnTrace("Context Switch");
-         _RunningProcess.state = "ready";
-         _RunningProcess.pc = _CPU.PC;
-         _RunningProcess.accumulator = _CPU.Acc;
-         _RunningProcess.xReg = _CPU.Xreg;
-         _RunningProcess.yReg = _CPU.Yreg;
-         _RunningProcess.zFlag = _CPU.Zflag;
+         if(_RunningProcess.state != "terminated"){
+            _RunningProcess.state = "ready";
+            _RunningProcess.pc = _CPU.PC;
+            _RunningProcess.accumulator = _CPU.Acc;
+            _RunningProcess.xReg = _CPU.Xreg;
+            _RunningProcess.yReg = _CPU.Yreg;
+            _RunningProcess.zFlag = _CPU.Zflag;
 
-         _ReadyQueue.enqueue(_RunningProcess);
+            _ReadyQueue.enqueue(_RunningProcess);
+         }
          _RunningProcess = _ReadyQueue.dequeue();
-         _RunningProcess.state = "running";
-         _CPU.switch(_RunningProcess);
+         if(_RunningProcess.slot >= 0){
+            _RunningProcess.state = "running";
+            _CPU.clear();
+            _CPU.switch(_RunningProcess);
+         }
+         else{
+            if(!_MemManager.isMemAvailable()){
+               var swapFile = _ReadyQueue.q[_ReadyQueue.getSize()-1]
+               if(swapFile)
+                  _MemManager.rollOut(swapFile);
+            }
+            _MemManager.rollIn(_RunningProcess);
+            _RunningProcess.state = "running";
+            _CPU.clear();
+            _CPU.switch(_RunningProcess);
+         }
       }
       _NumCycles = 0;
    }
